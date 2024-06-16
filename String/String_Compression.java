3cb3a8f2aa199970e04ac5f75d6d4a72a5310dd0
kavyaPriyadarshi <142102777+kavyaPriyadarshi@users.noreply.github.com>
2024-06-16 15:08:20 +0530

    Create String_Compression.java

---
+++ String/String_Compression.java
@@ -0,0 +1,69 @@
+/*
+Given an array of characters chars, compress it using the following algorithm:
+Begin with an empty string s. For each group of consecutive repeating characters in chars:
+If the group's length is 1, append the character to s.
+Otherwise, append the character followed by the group's length.
+The compressed string s should not be returned separately, but instead, be stored in the input character array chars. Note that group lengths that are 10 or longer will be split into multiple characters in chars.
+After you are done modifying the input array, return the new length of the array.
+You must write an algorithm that uses only constant extra space.
+
+Example 1:
+
+Input: chars = ["a","a","b","b","c","c","c"]
+Output: Return 6, and the first 6 characters of the input array should be: ["a","2","b","2","c","3"]
+Explanation: The groups are "aa", "bb", and "ccc". This compresses to "a2b2c3".
+*/
+
+class Solution {
+    public int compress(char[] chars) {
+        String s = "";
+        int count = 0;
+        char val = chars[0];
+        int index = 0;
+        int m = 0;
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == val) {
+                count++;
+            } else {
+                if (count > 1) {
+                    s = s + val + count;
+                    chars[index] = val;
+                    index++;
+                    String k = Integer.toString(count);
+                    m = 0;
+                    while (m < k.length()) {
+                        chars[index] = k.charAt(m);
+                        index++;
+                        m++;
+                    }
+                } else {
+                    s = s + chars[i];
+                    chars[index] = val;
+                    index++;
+                }
+                val = chars[i];
+                count = 1;
+            }
+        }
+        if (count == 1) {
+            s = s + val;
+            chars[index] = val;
+            index++;
+        } else {
+            chars[index] = val;
+            index++;
+            String k = Integer.toString(count);
+            m = 0;
+            while (m < k.length()) {
+                chars[index] = k.charAt(m);
+                index++;
+                m++;
+            }
+
+            s = s + val + count;
+        }
+
+        return s.length();
+
+    }
+}
