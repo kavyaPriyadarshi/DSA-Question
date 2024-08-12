3a9c626ead287001c5c26cd0b04ba573149b472f
kavyaPriyadarshi <142102777+kavyaPriyadarshi@users.noreply.github.com>
2024-08-12 14:21:22 +0530

    Create Kth_Largest_element_in_Stream.java

---
+++ Array/Kth_Largest_element_in_Stream.java
@@ -0,0 +1,52 @@
+/*
+Design a class to find the kth largest element in a stream. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+Implement KthLargest class:
+
+KthLargest(int k, int[] nums) Initializes the object with the integer k and the stream of integers nums.
+int add(int val) Appends the integer val to the stream and returns the element representing the kth largest element in the stream.
+ 
+
+Example 1:
+
+Input
+["KthLargest", "add", "add", "add", "add", "add"]
+[[3, [4, 5, 8, 2]], [3], [5], [10], [9], [4]]
+Output
+[null, 4, 5, 5, 8, 8]
+
+Explanation
+KthLargest kthLargest = new KthLargest(3, [4, 5, 8, 2]);
+kthLargest.add(3);   // return 4
+kthLargest.add(5);   // return 5
+kthLargest.add(10);  // return 5
+kthLargest.add(9);   // return 8
+kthLargest.add(4);   // return 8
+*/
+
+class KthLargest {
+    ArrayList<Integer> list = new ArrayList<>();
+    int m=0;
+
+    public KthLargest(int k, int[] nums) {
+        m=k;
+        for(int i=0;i<nums.length;i++){
+            list.add(nums[i]);
+        }
+        //list = Arrays.asList(nums);
+
+    }
+    
+    public int add(int val) {
+        list.add(val);
+        Collections.sort(list);
+        
+
+        if(list.size()!=0){
+        return list.get(list.size()-m);
+        }
+        return 1;
+        
+
+    }
+}
