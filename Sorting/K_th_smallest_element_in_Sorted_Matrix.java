779337cfca7fdab01fb0a2801ce08ee564f1b35d
kavyaPriyadarshi <142102777+kavyaPriyadarshi@users.noreply.github.com>
2024-08-24 21:29:11 +0530

    Create K_th_smallest_element_in_Sorted_Matrix.java

---
+++ Sorting/K_th_smallest_element_in_Sorted_Matrix.java
@@ -0,0 +1,38 @@
+/*
+378.
+Given an n x n matrix where each of the rows and columns is sorted in ascending order, return the kth smallest element in the matrix.
+
+Note that it is the kth smallest element in the sorted order, not the kth distinct element.
+
+You must find a solution with a memory complexity better than O(n2).
+
+ 
+
+Example 1:
+
+Input: matrix = [[1,5,9],[10,11,13],[12,13,15]], k = 8
+Output: 13
+Explanation: The elements in the matrix are [1,5,9,10,11,12,13,13,15], and the 8th smallest number is 13
+Example 2:
+
+Input: matrix = [[-5]], k = 1
+Output: -5
+*/
+
+class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+        PriorityQueue<Integer> pQueue = new PriorityQueue<Integer>(
+                Collections.reverseOrder());
+
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[i].length;j++){
+                pQueue.offer(matrix[i][j]);
+                
+                if(pQueue.size()>k){
+                    System.out.println(pQueue.poll());
+                }
+            }
+            
+            
+        }
+        return pQueue.peek();
